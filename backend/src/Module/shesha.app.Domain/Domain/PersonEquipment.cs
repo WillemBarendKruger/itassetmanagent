d673bb8b249c342ab312425d31fad70a8827cb7a
WillemBarendKruger <willem.kruger11@gmail.com>
2025-08-20 09:19:32 +0200

    feature - added new tables needed

---
+++ backend/src/Module/shesha.app.Domain/Domain/PersonEquipment.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Domain.Entities;
+using Shesha.Domain.Attributes;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace shesha.app.Domain.Domain
+{
+    [Table("vw_personsEquipment")]
+    [ImMutable]
+    public class PersonEquipment: Entity<Guid>
+    {
+        public virtual string FullName { get; set; }
+        public virtual string EmailAddress1 { get; set; }
+        public virtual int EquipmentCount { get; set; }
+    }
+}
