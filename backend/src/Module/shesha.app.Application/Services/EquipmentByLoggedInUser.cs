de37d9f18009fdd95279a693441d61766f087df9
WillemBarendKruger <willem.kruger11@gmail.com>
2025-08-21 15:05:22 +0200

    Feature - implemented equipment filter for employees

---
+++ backend/src/Module/shesha.app.Application/Services/EquipmentByLoggedInUser.cs
@@ -0,0 +1,34 @@
+﻿using Abp.Dependency;
+using Abp.Domain.Repositories;
+using Abp.Runtime.Session;
+using shesha.app.Domain.Domain;
+using Shesha.Domain;
+using Shesha.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shesha.app.Common.Services
+{
+    public class EquipmentByLoggedInUser : ShaSpecification<Equipment>
+    {
+            private readonly IAbpSession _abpSession;
+
+            public EquipmentByLoggedInUser(
+                IAbpSession abpSession)
+            {
+                _abpSession = abpSession;
+            }
+
+            public override Expression<Func<Equipment, bool>> BuildExpression()
+            {
+                var personRepo = IocManager.Resolve<IRepository<Person, Guid>>();
+                var currentPerson = personRepo.GetAll().FirstOrDefault(p => p.User != null && p.User.Id == AbpSession.UserId);
+
+                return equipmentItem => equipmentItem.Handler == currentPerson;
+            }
+    }
+}
